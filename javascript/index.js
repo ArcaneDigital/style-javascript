d8d7b85e4b9b46fca93e8c4449c7c04230be7e56
Jamieson Roberts <hello@jamiesonroberts.com>
2018-10-24 08:46:01 -0400

    Setting up configs to be able to test

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,11 @@
 module.exports = {
     'extends': ['airbnb'].map(require.resolve),
-    'rules': {}
+    'rules': {
+        'plugins': ['prettier'],
+        'rules': {
+            'indent': ['error', 4],
+            'max-len': ['error', { 'code': 80 }],
+            'prettier/prettier': ['error'],
+        },
+    },
 };
