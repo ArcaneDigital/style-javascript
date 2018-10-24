6d8659438effb2d8874338df736fda544dffbc38
Jamieson Roberts <hello@jamiesonroberts.com>
2018-10-24 16:31:04 -0400

    Correcting prettier config rule setup

--- javascript/prettier.config.js
+++ javascript/prettier.config.js
@@ -1,3 +1,19 @@
 module.exports = {
-    ...require('../prettier.config'),
+    overrides: [
+        {
+            files: ['.json', '.eslintrc'],
+            options: {
+                tabWidth: 2,
+            },
+        },
+        {
+            files: ['.js', '.jsx'],
+            options: {
+                singleQuote: true,
+                tabWidth: 4,
+                trailingComma: 'all',
+                arrowParens: 'avoid',
+            },
+        },
+    ],
 };
