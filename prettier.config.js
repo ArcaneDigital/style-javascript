8d52c72fdd6dae537cd8ba1a093c62fa01268d76
Jamieson Roberts <hello@jamiesonroberts.com>
2018-10-24 16:31:43 -0400

    Version bump and updating project wide prettier setup

--- prettier.config.js
+++ prettier.config.js
@@ -1,14 +1,5 @@
 module.exports = {
-    singleQuote: true,
-    tabWidth: 4,
-    trailingComma: 'all',
-    arrowParens: 'avoid',
-    overrides: [
-        {
-            files: ['*.json', '.eslintrc'],
-            options: {
-                tabWidth: 2,
-            },
-        },
-    ],
+    ...require.resolve('prettier.config.js', {
+        paths: ['./javascript', './css'],
+    }),
 };
